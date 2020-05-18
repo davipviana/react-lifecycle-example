8194b8d21d5c2d5b6fda90154daff3e534c7ff2d
Davi Viana <davipviana@gmail.com>
2020-05-18 17:47:56 -0300

    person message

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,21 +5,25 @@ import styles from './Cockpit.module.css';
 const Cockpit = (props) => {
     const assignedClasses = [];
     let btnClass = '';
+    let message = 'It\'s working';
+
     if (props.showPersons) {
         btnClass = styles.Red;
     }
 
     if (props.persons.length <= 2) {
-        assignedClasses.push(styles.red); // classes = ['red']
+        assignedClasses.push(styles.red);
+        message = 'Enough persons';
     }
     if (props.persons.length <= 1) {
-        assignedClasses.push(styles.bold); // classes = ['red', 'bold']
+        assignedClasses.push(styles.bold);
+        message = 'Sadly we have just few persons';
     }
 
     return (
         <div className={styles.Cockpit}>
             <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
+            <p className={assignedClasses.join(' ')}>{message}</p>
             <button
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
