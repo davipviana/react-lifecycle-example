c669c32460c539df81d10dde1d0bb1f6f8e9fc4a
Davi Viana <davipviana@gmail.com>
2020-05-18 17:23:24 -0300

    using css modules

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+        <div className={styles.App}>
+            <header>
+                <img src={logo} alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
         </p>
                 <a
-                    className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
