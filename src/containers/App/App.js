fc61e248c74cba629d36c7375a97eab51675e7fa
Davi Viana <davipviana@gmail.com>
2020-05-18 17:27:55 -0300

    app title

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
 import styles from './App.module.css';
 
-function App() {
+function App(props) {
     return (
         <div className={styles.App}>
-            <header>
-                <img src={logo} alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-        </p>
-                <a
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-            </header>
+            <h1>{props.title}</h1>
         </div>
     );
 }
