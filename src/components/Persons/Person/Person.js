1a831858bd50370d8a22ff9bd2da19cfbae42ddd
Davi Viana <davipviana@gmail.com>
2020-05-19 16:55:46 -0300

    tracks react component update

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './Person.module.css';
 
-const Person = props => {
-    console.log('[Person.js] rendering...');
-
-    return (
-        <div className={styles.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    );
+class Person extends Component {
+    render() {
+        console.log('[Person.js] rendering...');
+        return (
+            <div className={styles.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        );
+    }
 };
 
 export default Person;
